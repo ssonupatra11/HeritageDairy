56ce857a586ab500175acca7aaa5a297ad3edbfd
s.sonupatra99@ <s.sonupatra@gmail.com>
2023-04-10 22:24:04 +0530

    modified login fucntionality php file

--- modules/LoginFunctionality.php
+++ modules/LoginFunctionality.php
@@ -1,30 +1,48 @@
 <?php
+include_once("../modules/DatabaseConnection.php");
+include_once("../modules/ExitPage.php");
 include_once("../public/Home.php");
+
 class LoginFunctionality{
+    //function to check login email and password
     public function checkLogin(String $uemail,String $password){
-        $user_file=fopen("C:\Users\Dell\Desktop\HeritageDairy\modules\userdata.txt","r");
-        if(filesize("C:\Users\Dell\Desktop\HeritageDairy\modules\userdata.txt")>0){
-            $content=explode(",",fread($user_file,filesize("C:\Users\Dell\Desktop\HeritageDairy\modules\userdata.txt")));
-        }
-        fclose($user_file);
         $uem=null;
         $nam=null;
         $typ=null;
-        foreach($content as $val)
-        {
-            if(strlen($val)>0){
-                $v=explode(" ",$val);
-                if($v[1]===$uemail && $v[5]===$password){
-                    $uem=$uemail;
-                    $nam=$v[0];
-                    $typ="user";
-                }
-                elseif($v[1]===$uemail && $v[5]!=$password){
-                    echo "\nPassword is incorrect.Login again!\n";
-                    (new Home())->home();
+
+        //getting the connection
+        $conn=(new DatabaseConnection())->getConnection();
+
+        //try catch to handle sql exception
+        try{
+            $sql="Select name,email,password from user";
+            //getting records from user table
+            $result=$conn->query($sql);
+            if($result->num_rows>0){
+
+                //while loop to fetch rows one by one from result variable
+                while($row=$result->fetch_assoc()){
+                    if($row["email"]===$uemail && $row["password"]===$password){
+                        $uem=$uemail;
+                        $nam=$row["name"];
+                        $typ="user";
+                    }
+                    elseif($row["email"]===$uemail && $row[2]!=$password){
+                        echo "\nPassword is incorrect.Login again!\n";
+                        (new Home())->home();
+                    }
                 }
             }
         }
+        catch(mysqli_sql_exception $e){
+            throw new mysqli_sql_exception($sql,$e->getMessage(),$e->getCode());
+        }
+        finally{
+            //closing connection
+            $conn->close();
+        }
+
+        //if condition to check whether all sessions are null or not
         if($uem!=null && $nam!=null && $typ!=null){
             $_SESSION["email"]=$uem;
             $_SESSION["name"]=$nam;
@@ -35,8 +53,6 @@ class LoginFunctionality{
             echo "User is not available.Register new User.\n\n";
             (new Register())->addNewUser();
         }
-
-
     }
 }
 ?>
